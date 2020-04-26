7991c0e431948e5d0a7bd1fd9f7fc6645819e270
ctsahn <ctsahn@gmail.com>
2020-04-26 14:35:23 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="protein", 
+    version="1.0.2",
+    scripts=['protein'],
+    author="Curtis S. Ahn",
+    author_email="ctsahn@gmail.com",
+    license = "MIT",
+    keywords="protein bioinformatics uniprot",
+    description="Quick UniProt protein search for the command line",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ctsahn/protein",
+    packages=setuptools.find_packages(),
+    install_requires=['xmltodict'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+)
